bd671b836bff95b161282e58111fe0c2636ad711
GeorgeHBastos <georgehbastos@gmail.com>
2024-10-09 16:35:19 -0300

    teste fibonnacci

--- projetojs.js
+++ projetojs.js
@@ -8,3 +8,18 @@ while (K < INDICE) {
 }
 
 console.log(SOMA);
+
+"Sendo assim, a resposta pro problema é 91."
+
+function gerarFibonacci (teste) {
+let fibonacci = 1;
+let fibonnacci2 = [fibonacci.lenght - 1] + fibonacci [fibonacci.lenght - 2];
+while (fibonnacci2 <= teste) {
+fibonacci.push(fibonnacci2);
+
+}
+fibonnacci2 = fibonnacci[fibonacci[fibonacci.lenght -1]] + fibonacci [fibonacci.length - 2];
+
+}
+
+return fibonnacci;
